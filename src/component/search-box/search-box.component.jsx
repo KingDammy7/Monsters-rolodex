747d2778ff03d18ba1f9fdea2942affe19730340
unknown <contactdamilolaayodele@gmail.com>
2021-08-14 11:12:47 +0100

    adding react-app

---
+++ src/component/search-box/search-box.component.jsx
@@ -0,0 +1,13 @@
+import './search-box.styles.css';
+
+export const Searchbox = ({ placeholder, handleChange }) => (
+
+<input 
+    className= 'search'
+        type='search' 
+        placeholder= {placeholder} 
+        onChange={handleChange}
+           />
+
+
+);
