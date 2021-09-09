7893a0de1865114d9ce6b923e980facccfdc6c49
KingDammy7 <88653439+KingDammy7@users.noreply.github.com>
2021-09-09 14:06:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,17 +19,18 @@ class App extends Component {
   }
 
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monsters => 
-      monsters.name.toLowerCase().includes(searchField.toLowerCase())
+    const { monsters, searchField, title } = this.state;
+   // const filteredMonsters = monsters.filter(monsters => 
+     // monsters.name.toLowerCase().includes(searchField.toLowerCase())
       ) 
     return (
       <div className="App">
-        <h1> Monsters Rolodex </h1>
+        <h1> title </h1>
            <Searchbox
            placeholder= 'search monsters'
            handleChange= {e => {
-            this.setState({ searchField: e.target.value},
+            this.setState({ searchField: e.target.value,
+            title: event.target.value }
             );
           }}
            />
